e3677790b7e5af640d611b67b827382459f40e58
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-02-28 16:52:04 +0100

    refactorisation

--- Application/AttentionGet/Display/Form1.Designer.cs
+++ Application/AttentionGet/Display/Form1.Designer.cs
@@ -169,7 +169,7 @@
             this.KeyPreview = true;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "T-Rex Game";
+            this.Text = "DinoGame";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
